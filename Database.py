05d4b856e1ac58ab503adede9563c672564d574c
mlg325 <mlg325@lehigh.edu>
2023-06-22 00:24:26 -0400

    Added table

--- Database.py
+++ Database.py
@@ -1,44 +1,57 @@
-host = 'your_host'
-database = 'your_database'
-user = 'your_username'
-password = 'your_password'
+import requests
+import json
+import psycopg2
 
-
-
-conn = psycopg2.connect(
-    host=host,
-    database=database,
-    user=user,
-    password=password
-)
-
-
-cur = conn.cursor()
-
-
-url = 'http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1'
-response = requests.get(url)
+# Step 1: Retrieve the data
+response = requests.get("http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1")
 data = response.json()
 
-
+# Step 2: Parse the JSON
 games = data['dates'][0]['games']
+
+# Step 3: Extract the required fields
+records = []
 for game in games:
-    gamePk = game['gamePk']
-    gameType = game['gameType']
-    season = game['season']
-    gameDate = game['gameDate']
-    # Extract other relevant data fields as needed
+    game_id = game['gamePk']
+    team1 = game['teams']['away']['team']['name']
+    team2 = game['teams']['home']['team']['name']
+    game_date = game['gameDate']
+    start_time_tbd = game['status']['startTimeTBD']
+    venue = game['venue']['name']
+    records.append((game_id, team1, team2, game_date, start_time_tbd, venue))
 
-    # Insert the data into the database
-    cur.execute(
+# Step 4: Set up a connection to ElephantSQL
+conn = psycopg2.connect(
+    host = 'rajje.db.elephantsql.com',
+    database = 'syabkhtb',
+    user = 'syabkhtb',
+    port = '5432',
+    password = 'J7LXI5pNQ_UoUP316yEd-yoXnCOZK8HE'
+)
 
-        # ASSUMES THAT THERE IS A TABLE CALLED GAMES
-        # TODO - Create table "games", and change the parameters to be the ones that I want
-        "INSERT INTO games (gamePk, gameType, season, gameDate) VALUES (%s, %s, %s, %s)",
-        (gamePk, gameType, season, gameDate)
+# Step 5: Create a table
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS games (
+        id INT,
+        team1 VARCHAR(255),
+        team2 VARCHAR(255),
+        gameDate DATE,
+        startTimeTBD BOOLEAN,
+        venue VARCHAR(255),
+        PRIMARY KEY (id)
     )
+""")
 
-# Commit the changes and close the connection
 conn.commit()
-cur.close()
+
+# Step 6: Insert data into the table
+cursor.executemany("""
+    INSERT INTO games (id, team1, team2, gameDate, startTimeTBD, venue)
+    VALUES (%s, %s, %s, %s, %s, %s)
+""", records)
+conn.commit()
+
+# Close the connection
+cursor.close()
 conn.close()
