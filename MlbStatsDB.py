07f1b87a1eb5320c9b3c4bfa86bf3fc82c6b8c4c
mlg325 <mlg325@lehigh.edu>
2023-06-23 21:18:31 -0400

    removed the last table and moved to different file

--- MlbStatsDB.py
+++ MlbStatsDB.py
@@ -132,6 +132,10 @@ cursor.execute("""
 
 unique_games = []
 game_ids = set()
+starters = []
+lineup = []
+teamsLineup = []
+teamsStarters = []
 
 # Loop through each gameId and create rows in the "LineupAndProbables" table
 for game in games:
@@ -161,21 +165,46 @@ for game in unique_games:
         battingOrderAway = battingOrderAway_str
 
     probablePitcherHome = data['gameData']['probablePitchers']['home']
+    probablePitcherHomeId = data['gameData']['probablePitchers']['home']['id']
+    starters.append(probablePitcherHomeId)
+    teamsStarters.append(homeTeamName)
     probablePitcherAway = data['gameData']['probablePitchers']['away']
+    probablePitcherAwayId = data['gameData']['probablePitchers']['away']['id']
+    starters.append(probablePitcherAwayId)
+    teamsStarters.append(awayTeamName)
 
-
+    awayTeamName = awayTeam['name']
+    homeTeamName = homeTeam['name']
 
     # Check if battingOrderAway has at least 9 elements
     if len(battingOrderAway) >= 9:
         batterOneAway = battingOrderAway[0]
+        lineup.append(battingOrderAway[0])
+        teamsLineup.append(awayTeamName)
         batterTwoAway = battingOrderAway[1]
+        lineup.append(battingOrderAway[1])
+        teamsLineup.append(awayTeamName)
         batterThreeAway = battingOrderAway[2]
+        lineup.append(battingOrderAway[2])
+        teamsLineup.append(awayTeamName)
         batterFourAway = battingOrderAway[3]
+        lineup.append(battingOrderAway[3])
+        teamsLineup.append(awayTeamName)
         batterFiveAway = battingOrderAway[4]
+        lineup.append(battingOrderAway[4])
+        teamsLineup.append(awayTeamName)
         batterSixAway = battingOrderAway[5]
+        lineup.append(battingOrderAway[5])
+        teamsLineup.append(awayTeamName)
         batterSevenAway = battingOrderAway[6]
+        lineup.append(battingOrderAway[6])
+        teamsLineup.append(awayTeamName)
         batterEightAway = battingOrderAway[7]
+        lineup.append(battingOrderAway[7])
+        teamsLineup.append(awayTeamName)
         batterNineAway = battingOrderAway[8]
+        lineup.append(battingOrderAway[8])
+        teamsLineup.append(awayTeamName)
     else:
         # Set default values or handle the case where there are not enough elements
         batterOneAway = None
@@ -191,14 +220,32 @@ for game in unique_games:
     # Check if battingOrderHome has at least 9 elements
     if len(battingOrderHome) >= 9:
         batterOneHome = battingOrderHome[0]
+        lineup.append(battingOrderHome[0])
+        teamsLineup.append(homeTeamName)
         batterTwoHome = battingOrderHome[1]
+        lineup.append(battingOrderHome[1])
+        teamsLineup.append(homeTeamName)
         batterThreeHome = battingOrderHome[2]
+        lineup.append(battingOrderHome[2])
+        teamsLineup.append(homeTeamName)
         batterFourHome = battingOrderHome[3]
+        lineup.append(battingOrderHome[3])
+        teamsLineup.append(homeTeamName)
         batterFiveHome = battingOrderHome[4]
+        lineup.append(battingOrderHome[4])
+        teamsLineup.append(homeTeamName)
         batterSixHome = battingOrderHome[5]
+        lineup.append(battingOrderHome[5])
+        teamsLineup.append(homeTeamName)
         batterSevenHome = battingOrderHome[6]
+        lineup.append(battingOrderHome[6])
+        teamsLineup.append(homeTeamName)
         batterEightHome = battingOrderHome[7]
+        lineup.append(battingOrderHome[7])
+        teamsLineup.append(homeTeamName)
         batterNineHome = battingOrderHome[8]
+        lineup.append(battingOrderHome[8])
+        teamsLineup.append(homeTeamName)
     else:
         # Set default values or handle the case where there are not enough elements
         batterOneHome = None
@@ -379,11 +426,209 @@ for game in games:
 
 
 
-    # TODO - Create a table that shows individual stats - such as the starting pitchers stats, the individual batters in the lineup stats, etc.
-    # so for every player in game - return their hitting stats
-    # get hitting stats from https://statsapi.mlb.com/api/v1/people/{playerId}/stats?stats=byDateRange&season=2023&group=hitting&startDate=03/30/2023&endDate={currentDate}&leagueListId=mlb_milb
-    currentDate = datetime.now().strftime("%m/%d/%Y")
+# TODO - Create a table that shows individual stats - such as the starting pitchers stats, the individual batters in the lineup stats, etc.
+# so for every player in game - return their hitting stats
+# get hitting stats from https://statsapi.mlb.com/api/v1/people/{playerId}/stats?stats=byDateRange&season=2023&group=hitting&startDate=03/30/2023&endDate={currentDate}&leagueListId=mlb_milb
+currentDate = datetime.now().strftime("%m/%d/%Y")
 
+# Define the SQL statement to create the table
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS lineupStats (
+        player_id TEXT,
+        obp TEXT,
+        slg TEXT,
+        ops TEXT,
+        at_bats_per_home_run TEXT,
+        team_name TEXT,
+        games_played INTEGER,
+        babip TEXT
+    );
+""")
+
+
+# Loop through each player in the lineup
+for index, player_id in enumerate(lineup):
+    team_name = teamsLineup[index]  # Get the team name corresponding to the current player
+
+    # Make the API request to fetch player stats
+    api_url = "https://statsapi.mlb.com/api/v1/people/{playerId}/stats?stats=byDateRange&season=2023&group=hitting&startDate=03/30/2023&endDate={currentDate}&leagueListId=mlb_milb".format(
+        playerId=player_id,
+        currentDate=datetime.now().strftime("%m/%d/%Y")
+    )
+    response = requests.get(api_url)
+    data = response.json()
+
+    # Extract the required fields
+    stats = data["stats"][0]["splits"][0]["stat"]
+    games_played = stats["gamesPlayed"]
+    obp = stats["obp"]
+    slg = stats["slg"]
+    ops = stats["ops"]
+    at_bats_per_home_run = stats["atBatsPerHomeRun"]
+    babip = stats["babip"]
+
+    # Insert the player stats into the table
+    cursor.execute("""
+        INSERT INTO lineupStats (
+            player_id, obp, slg, ops, at_bats_per_home_run, team_name, games_played, babip
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    """, (
+        player_id, obp, slg, ops, at_bats_per_home_run, team_name, games_played, babip
+    ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TODO - Create a table that shows individual stats - such as the starting pitchers stats, the individual batters in the lineup stats, etc.
+# so for every player in game - return their pitching stats
+# get pitching stats from https://statsapi.mlb.com/api/v1/people/{playerId}/stats?stats=byDateRange&season=2023&group=pitching&startDate=03/30/2023&endDate={currentDate}&leagueListId=mlb_milb
+currentDate = datetime.now().strftime("%m/%d/%Y")
+
+# Define the SQL statement to create the table
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS probablesStats (
+        player_id TEXT,
+        strikeoutWalkRatio TEXT,
+        games_started INTEGER,
+        hitsPer9Inn TEXT,
+        strikeoutsPer9Inn TEXT,
+        team_name TEXT,
+        era TEXT,
+        whip TEXT,
+        walksPer9Inn TEXT
+    );
+""")
+
+
+# Loop through each player in starters
+for index, player_id in enumerate(starters):
+    team_name = teamsStarters[index]  # Get the team name corresponding to the current player
+
+    # Make the API request to fetch player stats
+    api_url = "https://statsapi.mlb.com/api/v1/people/{playerId}/stats?stats=byDateRange&season=2023&group=pitching&startDate=03/30/2023&endDate={currentDate}&leagueListId=mlb_milb".format(
+        playerId=player_id,
+        currentDate=datetime.now().strftime("%m/%d/%Y")  
+    )
+    response = requests.get(api_url)
+    data = response.json()
+
+    # Extract the required fields
+    stats = data["stats"][0]["splits"][0]["stat"]
+    strikeoutWalkRatio = stats["strikeoutWalkRatio"]
+    games_started = stats["gamesStarted"]
+    hitsPer9Inn = stats["hitsPer9Inn"]
+    strikeoutsPer9Inn = stats["strikeoutsPer9Inn"]
+    era = stats["era"]
+    whip = stats["whip"]
+    walksPer9Inn = stats["walksPer9Inn"]
+
+    # Insert the player stats into the table
+    cursor.execute("""
+        INSERT INTO probablesStats (
+            player_id, strikeoutWalkRatio, games_started, hitsPer9Inn, strikeoutsPer9Inn, team_name, era, whip, walksPer9Inn
+        )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, (
+        player_id, strikeoutWalkRatio, games_started, hitsPer9Inn, strikeoutsPer9Inn, team_name, era, whip, walksPer9Inn
+    ))
+
+
+
+
+
+# MAKE ANOTHER TABLE WITH LAST YEARS STATS -- USE THIS TABLE IF GAMES_STARTED IS LESS THAN 5
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS previousYearPitchingStats (
+        player_id TEXT,
+        strikeoutWalkRatio TEXT,
+        games_started INTEGER,
+        hitsPer9Inn TEXT,
+        strikeoutsPer9Inn TEXT,
+        team_name TEXT,
+        era TEXT,
+        whip TEXT,
+        walksPer9Inn TEXT
+    );
+""")
+     # SEE IF THERE IS A DIFFERENCE IF IT IS A TEXT OR FLOAT
+
+# Iterate over the starters
+for index, playerId in enumerate(starters):
+    team_name = teamsStarters[index]  # Get the team name corresponding to the current player
+    url = f"https://statsapi.mlb.com/api/v1/people/{playerId}/stats?stats=byDateRange&season=2022&group=pitching&startDate=05/07/2022&endDate=10/05/2022&leagueListId=mlb_milb"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    player_id = playerId
+
+    lastStatIndex = None  # Initialize lastStatIndex to None
+    foundStat = False  # Flag to track if a satisfying element is found
+
+    for idx, d in enumerate(data['stats']):
+        if 'stat' in d:
+            lastStatIndex = idx 
+            foundStat = True
+            break
+    print(lastStatIndex)
+    # Extract the required fields from the response
+    if foundStat:
+        stats = data['stats'][0][lastStatIndex]['stat']
+        strikeoutWalkRatio = stats["strikeoutWalkRatio"]
+        games_started = stats["gamesStarted"]
+        hitsPer9Inn = stats["hitsPer9Inn"]
+        strikeoutsPer9Inn = stats["strikeoutsPer9Inn"]
+        era = stats["era"]
+        whip = stats["whip"]
+        walksPer9Inn = stats["walksPer9Inn"]
+        pass
+    else :
+        stats = data["stats"][0]["splits"][0]["stat"]
+        strikeoutWalkRatio = stats["strikeoutWalkRatio"]
+        games_started = stats["gamesStarted"]
+        hitsPer9Inn = stats["hitsPer9Inn"]
+        strikeoutsPer9Inn = stats["strikeoutsPer9Inn"]
+        era = stats["era"]
+        whip = stats["whip"]
+        walksPer9Inn = stats["walksPer9Inn"]
+        pass
+
+    # Insert the player stats into the table
+    cursor.execute("""
+        INSERT INTO previousYearPitchingStats (
+            player_id, strikeoutWalkRatio, games_started, hitsPer9Inn, strikeoutsPer9Inn, team_name, era, whip, walksPer9Inn
+        )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, (
+        player_id, strikeoutWalkRatio, games_started, hitsPer9Inn, strikeoutsPer9Inn, team_name, era, whip, walksPer9Inn
+    ))
 
 
 
