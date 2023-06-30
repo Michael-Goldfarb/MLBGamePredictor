87e25d0d0ba51bbba99929dd74f1e681cc177373
mlg325 <mlg325@lehigh.edu>
2023-06-29 20:09:46 -0400

    Table works, but is slow and values aren't all right

--- Tables/UseTables.py
+++ Tables/UseTables.py
@@ -75,6 +75,10 @@ except FileNotFoundError:
 # Fetch the gameIds from the 'games' table
 query = "SELECT gameId FROM games"
 gameIds = pd.read_sql_query(query, engine)["gameid"].tolist()
+print(gameIds)
+
+# Create a list to store the updated data
+updated_data = []
 
 # Iterate over each gameId
 for gameId in gameIds:
@@ -119,167 +123,40 @@ for gameId in gameIds:
         print(f"GameId: {gameId} - No valid data available for prediction.")
         continue  # Skip to the next iteration of the loop
 
-    # Check if current_data_imputed is not empty
-    if not current_data_imputed.empty:
-        # Use the trained model to predict the winner of current date games
-        X_current = current_data_imputed.values
-        current_data["predicted_winner"] = model.predict(X_current)
+    # Use the trained model to predict the winner of current date games
+    X_current = current_data_imputed.values
+    current_data["predicted_winner"] = model.predict(X_current)
 
-        print("Column names in current_data:")
-        print(current_data.columns)
+    # Reset the index of the current_data DataFrame
+    current_data.reset_index(drop=True, inplace=True)
 
-        # Store the predicted winners in the database
-        current_data["predicted_winner"] = current_data["predicted_winner"].map({1: current_data["teamname"], 2: current_data["team_name"]})
-        predicted_winners = current_data["predicted_winner"].tolist()
+    # Store the predicted winners in the database
+    current_data.loc[current_data["predicted_winner"] == 1, "predicted_winner"] = current_data["teamid"]
 
-        for predicted_winner in predicted_winners:
-            update_query = f"UPDATE games SET predictedWinner = '{predicted_winner}' WHERE gameId = {gameId}"
-            engine.execute(update_query)
+    # Get the predicted winners as a list of integers
+    predicted_winners = current_data["predicted_winner"].astype(int).tolist()
 
-        print(f"GameId: {gameId} - Winner prediction stored in the database.")
-    else:
-        print(f"GameId: {gameId} - No valid data available for prediction.")
+    # Add the gameId and predicted winners to the updated_data list
+    updated_data.extend([(winner, gameId) for winner in predicted_winners])
 
-# Close the database connection
-engine.dispose()
+    print(f"GameId: {gameId} - Winner prediction stored in the database.")
 
+# Prepare the query to update all rows at once
+update_query = "UPDATE games SET predictedWinner = %(winner)s WHERE gameId = %(gameId)s"
 
+print("Im him")
 
+# Convert the updated_data list into a dictionary
+parameters = [{'winner': winner, 'gameId': gameId} for winner, gameId in updated_data]
 
-
-
-
-
-
-
-
-
-
-
-# from sqlalchemy import create_engine
-# import pandas as pd
-# import numpy as np
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.impute import SimpleImputer
-
-# # Create a SQLAlchemy engine
-# engine = create_engine('postgresql://syabkhtb:J7LXI5pNQ_UoUP316yEd-yoXnCOZK8HE@rajje.db.elephantsql.com:5432/syabkhtb')
-
-# # Fetch the historical data from the database tables
-# query = "SELECT * FROM PitchingStats2022"
-# pitching_stats_df = pd.read_sql_query(query, engine)
-
-# query = "SELECT * FROM probablesStats2022"
-# probables_stats_df = pd.read_sql_query(query, engine)
-
-# query = "SELECT * FROM previousYearPitchingStats2022"
-# previous_pitching_stats_df = pd.read_sql_query(query, engine)
-
-# query = "SELECT * FROM previousYearHittingStats2022"
-# previous_hitting_stats_df = pd.read_sql_query(query, engine)
-
-# query = "SELECT * FROM lineupStats2022"
-# lineup_stats_df = pd.read_sql_query(query, engine)
-
-# query = "SELECT * FROM HittingStats2022"
-# hitting_stats_df = pd.read_sql_query(query, engine)
-
-# # Merge the relevant historical tables based on common keys
-# historical_data = pd.merge(pitching_stats_df, probables_stats_df, on=["gameid"], how="inner", suffixes=('_pitching', '_probables'))
-# historical_data = pd.merge(historical_data, previous_pitching_stats_df, on=["gameid"], how="inner", suffixes=('', '_previous_pitching'))
-# historical_data = pd.merge(historical_data, previous_hitting_stats_df, on=["gameid"], how="inner", suffixes=('', '_previous_hitting'))
-# historical_data = pd.merge(historical_data, lineup_stats_df, on=["gameid"], how="inner", suffixes=('', '_lineup'))
-# historical_data = pd.merge(historical_data, hitting_stats_df, on=["gameid"], how="inner", suffixes=('', '_hitting'))
-
-# # Adjust the features list to include the correct column names
-# features = ["era", "whip", "hitsper9inn", "runsscoredper9", "homerunsper9",
-#             "strikeoutwalkratio_pitching", "games_started", "gamespitched", "strikeouts", "saves", "blownsaves",
-#             "obp", "slg_hitting", "ops", "strikeoutwalkratio_probables",
-#             "at_bats_per_home_run", "babip"]
-# target = "iswinner"
-
-# # Replace non-numeric values with NaN
-# historical_data[features] = historical_data[features].apply(pd.to_numeric, errors='coerce')
-
-# X = historical_data[features]
-# y = historical_data[target]
-
-# # Preprocess the data to handle missing values
-# imputer = SimpleImputer(strategy='mean')
-# X_imputed = imputer.fit_transform(X)
-
-# # Split the data into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.2, random_state=42)
-
-# # Train the logistic regression model
-# model = LogisticRegression(max_iter=2000)
-# model.fit(X_train, y_train)
-
-# # Fetch the current date data from the database tables
-# query = "SELECT * FROM PitchingStats"
-# current_pitching_stats_df = pd.read_sql_query(query, engine)
-
-# query = "SELECT * FROM probablesStats"
-# current_probables_stats_df = pd.read_sql_query(query, engine)
-
-# query = "SELECT * FROM previousYearPitchingStats"
-# current_previous_pitching_stats_df = pd.read_sql_query(query, engine)
-
-# query = "SELECT * FROM previousYearHittingStats"
-# current_previous_hitting_stats_df = pd.read_sql_query(query, engine)
-
-# query = "SELECT * FROM lineupStats"
-# current_lineup_stats_df = pd.read_sql_query(query, engine)
-
-# query = "SELECT * FROM HittingStats"
-# current_hitting_stats_df = pd.read_sql_query(query, engine)
-
-# # Merge the relevant current date tables based on common keys
-# current_data = pd.merge(current_pitching_stats_df, current_probables_stats_df, on=["gameid"], how="left", suffixes=('_pitching', '_probables'))
-# current_data = pd.merge(current_data, current_previous_pitching_stats_df, on=["gameid"], how="left", suffixes=('', '_previous_pitching'))
-# current_data = pd.merge(current_data, current_previous_hitting_stats_df, on=["gameid"], how="left", suffixes=('', '_previous_hitting'))
-# current_data = pd.merge(current_data, current_lineup_stats_df, on=["gameid"], how="left", suffixes=('', '_lineup'))
-# current_data = pd.merge(current_data, current_hitting_stats_df, on=["gameid"], how="left", suffixes=('', '_hitting'))
-
-# print("Current Data:")
-# print(current_data.head())
-
-# # Fill missing values with column means
-# numeric_columns = ["era", "whip", "hitsper9inn", "runsscoredper9", "homerunsper9",
-#                    "strikeoutwalkratio_pitching", "games_started", "gamespitched", "strikeouts", "saves", "blownsaves",
-#                    "obp", "slg_hitting", "ops", "strikeoutwalkratio_probables",
-#                    "at_bats_per_home_run", "babip"]
-# current_data[numeric_columns] = current_data[numeric_columns].fillna(current_data[numeric_columns].mean(numeric_only=True))
-
-
-# # Check for missing values in the current_data dataframe
-# missing_values = current_data[features].isnull().sum()
-# print(missing_values)
-
-# # Handle missing values in the current_data DataFrame
-# current_data[features] = current_data[features].apply(pd.to_numeric, errors='coerce')
-# current_data_imputed = imputer.transform(current_data[features])
-
-# # Check if current_data is not empty and has no missing values
-# if not current_data.empty:
-#     # Preprocess the current date data
-#     X_current = imputer.transform(current_data[features])
-
-#     # Use the trained model to predict the winner of current date games
-#     current_data["predicted_winner"] = model.predict(X_current)
-
-#     # Store the predicted winners in the database
-#     for index, row in current_data.iterrows():
-#         gameId = row["gameid"]
-#         predicted_winner = row["predicted_winner"]
-
-#         update_query = f"UPDATE games SET predictedWinner = {predicted_winner} WHERE gameid = {gameId}"
-#         engine.execute(update_query)
-
-#     print("Predictions have been stored in the database.")
-# else:
-#     print("No valid data available for prediction.")
-
-# # Close the database connection
-# engine.dispose()
+try:
+    # Execute the bulk update query
+    engine.execute(update_query, parameters)
+    print("Bulk update query executed.")
+except Exception as e:
+    # Handle any exceptions that occur during the execution
+    print("An error occurred during bulk update:", str(e))
+finally:
+    # Close the database connection
+    engine.dispose()
+    print("Database connection closed.")
