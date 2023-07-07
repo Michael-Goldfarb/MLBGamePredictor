c7a98056ec736dd40355d97886506b01aa18191e
mlg325 <mlg325@lehigh.edu>
2023-07-07 02:59:17 -0400

    Making front-end look good with backend

--- backend/src/main/java/com/pp/backend/controller/MLBGameController.java
+++ backend/src/main/java/com/pp/backend/controller/MLBGameController.java
@@ -1,21 +1,26 @@
 package com.pp.backend.controller;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.ArrayList;
+import com.pp.backend.entity.Prediction;
 import com.pp.backend.entity.MLBGameResponse;
 import com.pp.backend.entity.MLBGame;
 import com.pp.backend.service.MLBGameService;
+import com.pp.backend.service.PredictionService;
+
 
 @RestController
 @RequestMapping("/games")
 public class MLBGameController {
     private final MLBGameService mlbGameService;
+    private final PredictionService predictionService; // Add this line
 
-    public MLBGameController(MLBGameService mlbGameService) {
+    public MLBGameController(MLBGameService mlbGameService, PredictionService predictionService) { // Update the constructor
         this.mlbGameService = mlbGameService;
+        this.predictionService = predictionService; // Initialize the predictionService
     }
 
     @GetMapping
--- client/client/src/pages/PredictionsPage.js
+++ client/client/src/pages/PredictionsPage.js
@@ -8,7 +8,7 @@ const PredictionsPage = () => {
   useEffect(() => {
     const fetchPredictions = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/api/predictions')
+        const response = await axios.get('http://localhost:8000/games')
         console.log(response.data);
         setPredictions(response.data);
       } catch (error) {
@@ -22,11 +22,15 @@ const PredictionsPage = () => {
   return (
     <div className="predictions-page">
       <h1 className="predictions-title">MLB Game Predictions</h1>
-      {predictions.map((prediction) => (
-        <div key={prediction.gameId} className="prediction-item">
-          <h2>Game ID: {prediction.gameId}</h2>
-          <p>Early Winner: {prediction.earlyWinner}</p>
-          <p>Prediction: {prediction.prediction}</p>
+      {predictions.map((data) => (
+        <div key={data.mlbGame.gameId} className="prediction-item">
+          <p>Home Team: {data.mlbGame.homeTeamName}</p>
+          <p>Away Team: {data.mlbGame.awayTeamName}</p>
+          <p>Score: {data.mlbGame.homeTeamScore} - {data.mlbGame.awayTeamScore}</p>
+          {data.mlbGame.isWinnerHome || data.mlbGame.isWinnerAway ? <p>Final</p> : null}
+          <p>Date: {data.mlbGame.gameDate}</p>
+          <p>Time: {data.mlbGame.gameTime}</p>
+          <p>Prediction: {data.prediction?.prediction}</p>
         </div>
       ))}
     </div>
