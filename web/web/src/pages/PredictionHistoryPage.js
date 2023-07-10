5a0c897e5f9b4abaff18f302f935ed39457cf595
mlg325 <mlg325@lehigh.edu>
2023-07-09 22:07:27 -0400

    updating prediction history

--- web/web/src/pages/PredictionHistoryPage.js
+++ web/web/src/pages/PredictionHistoryPage.js
@@ -2,45 +2,43 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const PredictionHistoryPage = () => {
-    const [predictionHistory, setPredictionHistory] = useState(null);
-  
-    useEffect(() => {
-      const fetchPredictionHistory = async () => {
-        try {
-          const response = await axios.get('http://localhost:8000/api/prediction-history');
-          setPredictionHistory(response.data);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-  
-      fetchPredictionHistory();
-    }, []);
-  
-    const formatPredictionHistory = (predictionHistory) => {
-      if (!predictionHistory) {
-        return null;
+  const [predictionHistory, setPredictionHistory] = useState(null);
+
+  useEffect(() => {
+    const fetchPredictionHistory = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/prediction-history');
+        setPredictionHistory(response.data);
+      } catch (error) {
+        console.error(error);
       }
-  
-      const { predictionDate, numerator, denominator } = predictionHistory;
-  
-      const formattedDate = predictionDate.substring(5).replace('-', '/');
-      const formattedPrediction = `${numerator}/${denominator}`;
-  
-      return `Prediction History\n${formattedDate}: ${formattedPrediction}`;
     };
-  
-    return (
-      <div className="prediction-history-page">
-        <h1>Prediction History</h1>
-        {predictionHistory && (
-          <div className="prediction-history">
-            {formatPredictionHistory(predictionHistory)}
-          </div>
-        )}
-      </div>
-    );
+
+    fetchPredictionHistory();
+  }, []);
+
+  const formatPredictionHistory = (predictionHistory) => {
+    if (!predictionHistory) {
+      return null;
+    }
+
+    const { predictionDate, numerator, denominator } = predictionHistory;
+
+    const formattedDate = predictionDate.substring(5).replace('-', '/');
+    const formattedPrediction = `${numerator}/${denominator}`;
+
+    return `\n\nPrediction History\n\n${formattedDate}: ${formattedPrediction}`;
   };
-  
-  export default PredictionHistoryPage;
-  
+
+  return (
+    <div className="prediction-history-page">
+      {predictionHistory && (
+        <div className="prediction-history">
+          <p>{formatPredictionHistory(predictionHistory)}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PredictionHistoryPage;
