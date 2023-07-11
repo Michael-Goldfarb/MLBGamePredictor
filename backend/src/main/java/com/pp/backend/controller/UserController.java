9bbf4777e1402c33028a4e9819edca6c8f6bf67a
mlg325 <mlg325@lehigh.edu>
2023-07-11 01:48:02 -0400

    Testing to see if need the user class

--- backend/src/main/java/com/pp/backend/controller/UserController.java
+++ backend/src/main/java/com/pp/backend/controller/UserController.java
@@ -1,34 +1,34 @@
-package com.pp.backend.controller;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import java.util.List;
-import com.pp.backend.entity.User;
-import com.pp.backend.service.UserService;
+// package com.pp.backend.controller;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import java.util.List;
+// import com.pp.backend.entity.User;
+// import com.pp.backend.service.UserService;
 
-@RestController
-@RequestMapping("/users")
-public class UserController {
-    private final UserService userService;
+// @RestController
+// @RequestMapping("/users")
+// public class UserController {
+//     private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
 
-    @GetMapping
-    public ResponseEntity<List<User>> getUsers() {
-        List<User> users = userService.getUsers();
-        return ResponseEntity.ok(users);
-    }
+//     @GetMapping
+//     public ResponseEntity<List<User>> getUsers() {
+//         List<User> users = userService.getUsers();
+//         return ResponseEntity.ok(users);
+//     }
 
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User createdUser = userService.createUser(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
-    }
+//     @PostMapping
+//     public ResponseEntity<User> createUser(@RequestBody User user) {
+//         User createdUser = userService.createUser(user);
+//         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+//     }
 
-}
+// }
--- backend/src/main/java/com/pp/backend/entity/User.java
+++ backend/src/main/java/com/pp/backend/entity/User.java
@@ -1,33 +1,33 @@
-package com.pp.backend.entity;
-import com.fasterxml.jackson.annotation.JsonProperty;
+// package com.pp.backend.entity;
+// import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class User {
-    @JsonProperty("email")
-    private String email;
-    @JsonProperty("name")
-    private String name;
+// public class User {
+//     @JsonProperty("email")
+//     private String email;
+//     @JsonProperty("name")
+//     private String name;
 
-    public User() {
-    }
+//     public User() {
+//     }
 
-    public User(String email, String name) {
-        this.email = email;
-        this.name = name;
-    }
+//     public User(String email, String name) {
+//         this.email = email;
+//         this.name = name;
+//     }
 
-    public String getEmail() {
-        return email;
-    }
+//     public String getEmail() {
+//         return email;
+//     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
 
-    public String getName() {
-        return name;
-    }
+//     public String getName() {
+//         return name;
+//     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-}
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+// }
