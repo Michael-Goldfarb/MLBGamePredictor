7ca34b795faf33acd9c09064484247f9cdb58670
mlg325 <mlg325@lehigh.edu>
2023-07-11 15:25:14 -0400

    Made prediction history and team records two separate columns, more visually appealing

--- web/web/src/pages/PredictionHistoryPage.js
+++ web/web/src/pages/PredictionHistoryPage.js
@@ -51,30 +51,36 @@ const PredictionHistoryPage = () => {
   };
 
   return (
-    <div className="prediction-history-container text-white text-center">
+    <div className="prediction-history-page bg-gray-800 min-h-screen flex flex-col items-center justify-start py-10">
+  <div className="container flex mr-12">
+    <div className="left-section w-3/4 text-center">
       <h1 className="text-4xl font-bold mb-4">Prediction History</h1>
       {predictionHistory && (
         <ul className="prediction-list">
           {formatPredictionHistory(predictionHistory).split('\n').map((prediction, index) => (
-            <li key={index} className="mb-2">
+            <li key={index} className="mb-3">
               {prediction}
             </li>
           ))}
         </ul>
       )}
+    </div>
+    <div className="right-section w-1/2 text-center">
+      <h1 className="text-4xl font-bold mb-4">Team Records</h1>
       {teamRecords && (
-        <>
-          <h2 className="text-2xl font-bold mt-4">Team Records</h2>
-          <ul className="prediction-list">
-            {formatTeamRecords(teamRecords).split('\n').map((teamRecord, index) => (
-              <li key={index} className="mb-2">
-                {teamRecord}
-              </li>
-            ))}
-          </ul>
-        </>
+        <ul className="prediction-list">
+          {formatTeamRecords(teamRecords).split('\n').map((teamRecord, index) => (
+            <li key={index} className="mb-3">
+              {teamRecord}
+            </li>
+          ))}
+        </ul>
       )}
     </div>
+  </div>
+</div>
+
+
   );
 };
 
