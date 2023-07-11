54cf143be97ae4f8dc9ee6e3ca72ccf7873bb1b9
mlg325 <mlg325@lehigh.edu>
2023-07-11 15:55:31 -0400

    added buttons to sort by percentage

--- web/web/src/pages/PredictionHistoryPage.js
+++ web/web/src/pages/PredictionHistoryPage.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './PredictionHistoryPage.css';
 
 const PredictionHistoryPage = () => {
   const [predictionHistory, setPredictionHistory] = useState(null);
   const [teamRecords, setTeamRecords] = useState(null);
+  const [sortType, setSortType] = useState(''); // Track the current sort type
 
   useEffect(() => {
     const fetchPredictionHistory = async () => {
@@ -42,19 +44,36 @@ const PredictionHistoryPage = () => {
     if (!teamRecords || teamRecords.length === 0) {
       return null;
     }
-    const sortedTeamRecords = teamRecords.sort((a, b) => a.teamName.localeCompare(b.teamName)); // Sort the teamRecords array alphabetically by teamName
+
+    let sortedTeamRecords = [...teamRecords]; // Create a copy of the teamRecords array
+
+    // Sort the teamRecords based on the current sort type
+    if (sortType === 'highest') {
+      sortedTeamRecords.sort((a, b) => b.percentage - a.percentage);
+    } else if (sortType === 'lowest') {
+      sortedTeamRecords.sort((a, b) => a.percentage - b.percentage);
+    }
+
     return sortedTeamRecords.map((teamRecord) => {
       const { teamName, numerator, denominator, percentage } = teamRecord;
-      const formattedPercentage = (percentage * 100).toFixed(2); // Multiply by 100 and format to 2 decimal places
+      const formattedPercentage = (percentage * 100).toFixed(2);
       return `${teamName}: ${numerator}/${denominator} (${formattedPercentage}%)`;
     }).join('\n');
   };
 
+  const handleSortByHighest = () => {
+    setSortType('highest');
+  };
+
+  const handleSortByLowest = () => {
+    setSortType('lowest');
+  };
+
   return (
-    <div className="prediction-history-page bg-gray-800 min-h-screen flex flex-col items-center justify-start py-10">
+    <div className="prediction-history-page bg-gray-800 min-h-screen flex flex-col items-center justify-center py-10">
   <div className="container flex mr-12">
-    <div className="left-section w-3/4 text-center">
-      <h1 className="text-4xl font-bold mb-4">Prediction History</h1>
+    <div className="left-section w-3/4 text-center column prediction-history-column">
+      <h1 className="column-heading">Prediction History</h1>
       {predictionHistory && (
         <ul className="prediction-list">
           {formatPredictionHistory(predictionHistory).split('\n').map((prediction, index) => (
@@ -65,8 +84,16 @@ const PredictionHistoryPage = () => {
         </ul>
       )}
     </div>
-    <div className="right-section w-1/2 text-center">
-      <h1 className="text-4xl font-bold mb-4">Team Records</h1>
+    <div className="right-section w-1/2 text-center column">
+      <h1 className="column-heading">Team Records</h1>
+      <div className="sort-button-container">
+        <button className="sort-button" onClick={handleSortByHighest}>
+          Sort by Highest
+        </button>
+        <button className="sort-button" onClick={handleSortByLowest}>
+          Sort by Lowest
+        </button>
+      </div>
       {teamRecords && (
         <ul className="prediction-list">
           {formatTeamRecords(teamRecords).split('\n').map((teamRecord, index) => (
@@ -79,7 +106,6 @@ const PredictionHistoryPage = () => {
     </div>
   </div>
 </div>
-
 
   );
 };
