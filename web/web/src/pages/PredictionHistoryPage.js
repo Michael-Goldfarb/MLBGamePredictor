a8b6b202f1799a0e0d3383cc815e250ee7eb80f4
mlg325 <mlg325@lehigh.edu>
2023-07-14 19:30:45 -0400

    Updated front end to work with new backend changes for prediction history

--- web/web/src/pages/PredictionHistoryPage.js
+++ web/web/src/pages/PredictionHistoryPage.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './PredictionHistoryPage.css';
 
 const PredictionHistoryPage = () => {
-  const [predictionHistory, setPredictionHistory] = useState(null);
+  const [predictionHistory, setPredictionHistory] = useState([]);
   const [teamRecords, setTeamRecords] = useState(null);
   const [sortType, setSortType] = useState(''); // Track the current sort type
 
@@ -31,13 +31,15 @@ const PredictionHistoryPage = () => {
   }, []);
 
   const formatPredictionHistory = (predictionHistory) => {
-    if (!predictionHistory) {
+    if (!predictionHistory || predictionHistory.length === 0) {
       return null;
     }
-    const { predictionDate, numerator, denominator } = predictionHistory;
-    const formattedDate = predictionDate.substring(5).replace('-', '/');
-    const formattedPrediction = `${formattedDate}: ${numerator}/${denominator}`;
-    return formattedPrediction;
+
+    return predictionHistory.map((prediction) => {
+      const { predictionDate, numerator, denominator } = prediction;
+      const formattedDate = predictionDate.substring(5).replace('-', '/');
+      return `${formattedDate}: ${numerator}/${denominator}`;
+    });
   };
 
   const formatTeamRecords = (teamRecords) => {
@@ -58,7 +60,7 @@ const PredictionHistoryPage = () => {
       const { teamName, numerator, denominator, percentage } = teamRecord;
       const formattedPercentage = (percentage * 100).toFixed(2);
       return `${teamName}: ${numerator}/${denominator} (${formattedPercentage}%)`;
-    }).join('\n');
+    });
   };
 
   const handleSortByHighest = () => {
@@ -71,41 +73,41 @@ const PredictionHistoryPage = () => {
 
   return (
     <div className="prediction-history-page bg-gray-800 min-h-screen flex flex-col items-center justify-start py-10">
-  <div className="container flex mr-12">
-    <div className="left-section w-3/4 text-center">
-      <h1 className="text-4xl font-bold mb-4">Prediction History</h1>
-      {predictionHistory && (
-        <ul className="prediction-list">
-          {formatPredictionHistory(predictionHistory).split('\n').map((prediction, index) => (
-            <li key={index} className="mb-3">
-              {prediction}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-    <div className="right-section w-1/2 text-center">
-      <h1 className="text-4xl font-bold mb-4">Prediction Rate By Team</h1>
-      <div className="sort-button-container">
-        <button className="sort-button" onClick={handleSortByHighest}>
-          Sort by Highest Percentage
-        </button>
-        <button className="sort-button" onClick={handleSortByLowest}>
-          Sort by Lowest Percentage
-        </button>
+      <div className="container flex mr-12">
+        <div className="left-section w-3/4 text-center">
+          <h1 className="text-4xl font-bold mb-4">Prediction History</h1>
+          {predictionHistory && (
+            <ul className="prediction-list">
+              {formatPredictionHistory(predictionHistory).map((prediction, index) => (
+                <li key={index} className="mb-3">
+                  {prediction}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="right-section w-1/2 text-center">
+          <h1 className="text-4xl font-bold mb-4">Prediction Rate By Team</h1>
+          <div className="sort-button-container">
+            <button className="sort-button" onClick={handleSortByHighest}>
+              Sort by Highest Percentage
+            </button>
+            <button className="sort-button" onClick={handleSortByLowest}>
+              Sort by Lowest Percentage
+            </button>
+          </div>
+          {teamRecords && (
+            <ul className="prediction-list">
+              {formatTeamRecords(teamRecords).map((teamRecord, index) => (
+                <li key={index} className="mb-3">
+                  {teamRecord}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
-      {teamRecords && (
-        <ul className="prediction-list">
-          {formatTeamRecords(teamRecords).split('\n').map((teamRecord, index) => (
-            <li key={index} className="mb-3">
-              {teamRecord}
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
-  </div>
-</div>
   );
 };
 
